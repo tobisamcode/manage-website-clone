513a4fbf90ecb4dd84cc6f02d23f22569fdad6d4
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-28 00:19:55 +0100

    adding animations

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -4,12 +4,16 @@
     <!-- flex container  -->
     <div class="contaner flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
       <!-- heading  -->
-      <h2 class="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left">
+      <h2 data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" class="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left">
          Simplify how your team works today.
       </h2>
 
     <!-- Button  -->
-      <a href="#" class="p-3 px-6 text-brightRed bg-white rounded-full shadow-2xl baseline hover:bg-gray-900">Get Started</a>
+      <a href="#" data-aos="fade-left"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" class="p-3 px-6 text-brightRed bg-white rounded-full shadow-2xl baseline hover:bg-gray-900">Get Started</a>
     </div>
   </section>
 </template>

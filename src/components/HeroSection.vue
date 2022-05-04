fcb3ab3571b687add207800a7311a55d79e3459a
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-04 21:09:29 +0100

    adding animations

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -5,7 +5,7 @@
     <div class="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
       <!-- Left-item  -->
       <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
-        <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left animate__animated animate__fadeInBottomLeft animate__delay-2s">
+        <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
           Bring everyone together to build better products.
         </h1>
         <p data-aos="zoom-in" data-aos-delay="1500" class="max-w-sm text-center text-DarkGrayishBlue md:text-left">
